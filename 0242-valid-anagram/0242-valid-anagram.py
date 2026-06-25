088bef97dff857385eb69018e481f902dab8f8cd
shivenchauhan1 <sps012032@gmail.com>
2026-06-25 14:40:19 +0530

    Time: 23 ms (50.49%), Space: 13.5 MB (29.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,13 +6,4 @@ class Solution(object):
         :rtype: bool
         """
         
-        char1 = list(s.lower())
-        char2 = list(t.lower())
-
-        char1.sort()
-        char2.sort()
-
-        if len(char1) == len(char2) and char1 == char2:
-            return True
-        else:
-            return False
+        return sorted(s.lower()) == sorted(t.lower())
