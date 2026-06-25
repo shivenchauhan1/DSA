ced03396cd6688dd38d4dc848ff2f8de26566d79
shivenchauhan1 <sps012032@gmail.com>
2026-06-25 11:44:47 +0530

    Time: 15 ms (6.12%), Space: 47.7 MB (25.06%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+	    int n=matrix[0].length;
+	    ArrayList<Integer> a = new ArrayList<>();
+	    ArrayList<Integer> b = new ArrayList<>();
+	    for(int i=0;i<m;i++){
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j]==0){
+	                a.add(i);
+	                b.add(j);
+	            }
+	        }
+	    }
+	    for(int k=0;k<a.size();k++){
+            int row = a.get(k);
+            for(int j=0;j<n;j++){
+                matrix[row][j]=0;
+            }
+        }
+        for(int k=0;k<b.size();k++){
+            int col = b.get(k);
+            for(int i=0;i<m;i++){
+                matrix[i][col]=0;
+            }
+        }
+        int[][] copy = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            copy[i] = new int[matrix[i].length];
+            System.arraycopy(matrix[i], 0, copy[i], 0, matrix[i].length);
+        }
+    }
+}
