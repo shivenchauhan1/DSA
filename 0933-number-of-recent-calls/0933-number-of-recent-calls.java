ede4ad248c95179e277d8fc40654d031db801da5
shivenchauhan1 <sps012032@gmail.com>
2026-07-17 09:38:39 +0530

    Time: 22 ms (55.89%), Space: 59.7 MB (37.3%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,11 +1,9 @@
 class RecentCounter {
     Queue<Integer> q = new LinkedList<>();
-    public RecentCounter() {
-    }
     
     public int ping(int t){
         q.add(t);
-        while(!q.isEmpty() && q.peek() < t - 3000){
+        while(q.peek() < t - 3000){
             q.poll();
         }
         return q.size();
